737c9a92dc277ad42144029be467d3813b02058c
peterdemartini <thepeterdemartini@gmail.com>
2019-03-07 12:32:55 -0700

    Make ssl parameter's not required

--- packages/terafoundation_kafka_connector/src/schema.ts
+++ packages/terafoundation_kafka_connector/src/schema.ts
@@ -11,27 +11,27 @@ export = {
     },
     ssl_crl_location: {
         doc: 'Path to CRL for verifying broker\'s certificate validity',
-        default: null,
-        format: String
+        default: undefined,
+        format: 'optional_String'
     },
     ssl_ca_location: {
         doc: 'File or directory path to CA certificate(s) for verifying the broker\'s key',
-        default: null,
-        format: String
+        default: undefined,
+        format: 'optional_String'
     },
     ssl_certificate_location: {
         doc: 'Path to client\'s public key (PEM) used for authentication',
-        default: null,
-        format: String
+        default: undefined,
+        format: 'optional_String'
     },
     ssl_key_location: {
         doc: 'Path to client\'s private key (PEM) used for authentication',
-        default: null,
-        format: String
+        default: undefined,
+        format: 'optional_String'
     },
     ssl_key_password: {
         doc: 'Private key passphrase',
-        default: null,
-        format: String
+        default: undefined,
+        format: 'optional_String'
     }
 };
