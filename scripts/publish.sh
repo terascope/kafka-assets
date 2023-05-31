91298929cc2416b919bae8493a961647995fa93a
Jared Noble <jsnoble1@gmail.com>
2023-05-31 09:44:36 -0700

    log info command

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -20,7 +20,7 @@ publish() {
 
     targetVersion="$(jq -r '.version' package.json)"
     currentVersion="$(npm info --json 2> /dev/null | jq -r '.version // "0.0.0"')"
-
+    echo "$(npm info --json)"
     if [ "$currentVersion" != "$targetVersion" ]; then
         echo "$name@$currentVersion -> $targetVersion"
         if [ "$dryRun" == "false" ]; then
