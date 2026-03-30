cc6746e312d6cc09012f71219a9e7bed6bbdbcfc
sotojn <jsoto@terascope.com>
2026-03-30 15:11:53 -0700

    fix other errors

--- test/e2e/config.ts
+++ test/e2e/config.ts
@@ -1,12 +1,14 @@
 import { SchemaValidator } from '@terascope/core-utils';
+import { Terafoundation } from '@terascope/types';
 
 type E2EConfig = {
     TERASLICE_HOST: string;
     ASSET_ZIP_PATH: string;
     KAFKA_BROKER: string;
+    TEST_TERASLICE: boolean;
 };
 
-const schema = {
+const schema: Terafoundation.Schema<any> = {
     TERASLICE_HOST: {
         default: null,
         format: 'required_string'
@@ -18,9 +20,13 @@ const schema = {
     KAFKA_BROKER: {
         default: null,
         format: 'required_string'
-    }
+    },
+    TEST_TERASLICE: {
+        default: false,
+        format: Boolean,
+    },
 };
 
-const validator = new SchemaValidator<E2EConfig>(schema, 'e2e-config');
+const validator = new SchemaValidator<E2EConfig>(schema, 'e2e-config', undefined, 'allow');
 
 export default validator.validate(process.env);
